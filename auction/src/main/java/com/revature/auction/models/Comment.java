0ce349ad976ee206b23fa5196058fc11a1177d10
Daniel Thompson <DThompson124@my.gcu.edu>
2024-08-08 13:00:07 -0700

    Added Model Skeletons

---
+++ auction/src/main/java/com/revature/auction/models/Comment.java
@@ -0,0 +1,10 @@
+package com.revature.auction.models;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+public class Comment
+{
+}
---
+++ auction/src/main/java/com/revature/auction/models/User.java
@@ -0,0 +1,11 @@
+package com.revature.auction.models;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+public class User
+{
+
+}
