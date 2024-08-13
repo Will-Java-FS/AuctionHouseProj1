376695fc82fc58d357bb0b5fc2b560d04a9aaff2
Daniel Thompson <DThompson124@my.gcu.edu>
2024-08-13 11:56:33 -0700

    Got the application running again

--- auction/src/main/java/com/revature/auction/controllers/ItemController.java
+++ auction/src/main/java/com/revature/auction/controllers/ItemController.java
@@ -1,9 +1,13 @@
 package com.revature.auction.controllers;
 
+import com.revature.auction.models.Item;
 import com.revature.auction.services.ItemServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/item")
@@ -16,34 +20,36 @@ public class ItemController
     {
         this.itemService = itemService;
     }
-    @PostMapping("/add", consumes = "application/json",produces ="application/json")
+
+    @PostMapping(name="/add", consumes = "application/json",produces ="application/json")
     public ResponseEntity<Object> addItem(@RequestBody Item it)
     {
-        return itemService.addItem(it);
+        return new ResponseEntity<>(itemService.add(it), HttpStatus.OK);
     }
 
     @GetMapping
     public List<Item> getAllItems()
     {
-        return itemService.getAllItems();
+        return itemService.getAll();
     }
 
     @GetMapping("/{id}")
     public Item getById(@PathVariable int id)
     {
-        return itemService.getById(id);
+        return itemService.findById(id);
     }
 
-    @PatchMapping("/update/{id}", consumes = "application/json",produces ="application/json")
-    public ResponseEntity<Object> updateItem(@RequestBody Item it, @PathVariable id)
+    @PatchMapping(name="/update/{id}", consumes = "application/json",produces ="application/json")
+    public ResponseEntity<Item> updateItem(@RequestBody Item it, @PathVariable int id)
     {
-        return itemService.update(id, it);
+        return new ResponseEntity<>(itemService.update(id, it), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
     public Item deleteById(@PathVariable int id)
     {
-        return itemService.deleteById(id);
+        itemService.delete(id);
+        return null;
     }
 
 }
--- auction/src/main/java/com/revature/auction/repositories/BidRepo.java
+++ auction/src/main/java/com/revature/auction/repositories/BidRepo.java
@@ -1,15 +1,17 @@
 package com.revature.auction.repositories;
 
 import com.revature.auction.models.Bid;
+import com.revature.auction.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BidRepo extends JpaRepository<Bid, Integer>
 {
-    @Query("Select Items.* from Items Right Join Bids On Items.item_id = Bids.item_bidOn Where Bids.bid_id = ?1")
-    Bid findItemByBidId(int id);
-
-    @Query("Select Users.* from Users Right Join Bids On Users.user_id = Bids.user_bidder Where Bids.bid_id = ?1")
-    User findBidderByBidId(int id);
+//    @Query("Select Items.* from Items Right Join Bids On Items.item_id = Bids.item_bidOn Where Bids.bid_id = ?1")
+//    Bid findItemByBidId(int id);
+//
+//    @Query("Select Users.* from Users Right Join Bids On Users.user_id = Bids.user_bidder Where Bids.bid_id = ?1")
+//    User findBidderByBidId(int id);
 }
