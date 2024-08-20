1c069941ea9efd1a9a1ef64673f90bafe470e6ff
Daniel Thompson <DThompson124@my.gcu.edu>
2024-08-20 12:26:41 -0700

    Finished single-item page basic funtionality; Touched up details of home page

--- frontend/src/components/pages/home/home.jsx
+++ frontend/src/components/pages/home/home.jsx
@@ -1,125 +1,97 @@
-import {Card, CardBody, Image, Button} from "@nextui-org/react";
+import { Card, CardBody, Image, Button } from "@nextui-org/react";
 import { EyeIcon, PlusIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 function Home() {
+  const [items, setItems] = useState([]);
+  const [bids, setBids] = useState([]); // State to store bids
+  const [loading, setLoading] = useState(true); // State to manage loading state
+  const [error, setError] = useState(null); // State to manage errors
 
-  // const items = [
-  //   {
-  //     itemId: 1,
-  //     userOwner: "User",
-  //     itemName: "Cool Cat",
-  //     itemDescription: "Lorem ipsum odor amet, consectetuer adipiscing elit. Inceptos primis pretium mus adipiscing lacus nascetur volutpat. Litora sollicitudin facilisis hac mi efficitur taciti risus torquent. Duis nascetur class magna mauris phasellus. Nam etiam ante tempor hac blandit ultricies nulla congue quis. Malesuada a lacus fermentum erat eros sodales. Laoreet orci parturient vivamus libero suscipit natoque. Lacus iaculis ultricies inceptos sed ac tempor. Sodales eros sem ante cursus enim eget ex orci. Nunc luctus lobortis sem orci id cursus morbi.",
-  //     itemImage: require("../../../img/items/test/01.jpg"),
-  //     bidAmount: 15
-  //   },
-  //   {
-  //     itemId: 2,
-  //     userOwner: "User",
-  //     itemName: "Fancy Car",
-  //     itemDescription: "Lorem ipsum odor amet, consectetuer adipiscing elit. Inceptos primis pretium mus adipiscing lacus nascetur volutpat. Litora sollicitudin facilisis hac mi efficitur taciti risus torquent. Duis nascetur class magna mauris phasellus. Nam etiam ante tempor hac blandit ultricies nulla congue quis. Malesuada a lacus fermentum erat eros sodales. Laoreet orci parturient vivamus libero suscipit natoque. Lacus iaculis ultricies inceptos sed ac tempor. Sodales eros sem ante cursus enim eget ex orci. Nunc luctus lobortis sem orci id cursus morbi.",
-  //     itemImage: require("../../../img/items/test/02.jpg"),
-  //     bidAmount: 95
-  //   },
-  //   {
-  //     itemId: 3,
-  //     userOwner: "User",
-  //     itemName: "Poor People's Plane",
-  //     itemDescription: "Lorem ipsum odor amet, consectetuer adipiscing elit. Inceptos primis pretium mus adipiscing lacus nascetur volutpat. Litora sollicitudin facilisis hac mi efficitur taciti risus torquent. Duis nascetur class magna mauris phasellus. Nam etiam ante tempor hac blandit ultricies nulla congue quis. Malesuada a lacus fermentum erat eros sodales. Laoreet orci parturient vivamus libero suscipit natoque. Lacus iaculis ultricies inceptos sed ac tempor. Sodales eros sem ante cursus enim eget ex orci. Nunc luctus lobortis sem orci id cursus morbi.",
-  //     itemImage: require("../../../img/items/test/03.jpg"),
-  //     bidAmount: 1120
-  //   },
-  //   {
-  //     itemId: 4,
-  //     userOwner: "User",
-  //     itemName: "Totally Legal Gun",
-  //     itemDescription: "Lorem ipsum odor amet, consectetuer adipiscing elit. Inceptos primis pretium mus adipiscing lacus nascetur volutpat. Litora sollicitudin facilisis hac mi efficitur taciti risus torquent. Duis nascetur class magna mauris phasellus. Nam etiam ante tempor hac blandit ultricies nulla congue quis. Malesuada a lacus fermentum erat eros sodales. Laoreet orci parturient vivamus libero suscipit natoque. Lacus iaculis ultricies inceptos sed ac tempor. Sodales eros sem ante cursus enim eget ex orci. Nunc luctus lobortis sem orci id cursus morbi.",
-  //     itemImage: require("../../../img/items/test/04.jpg"),
-  //     bidAmount: 666
-  //   },
-  //   {
-  //     itemId: 5,
-  //     userOwner: "User",
-  //     itemName: "Absolutely Illegal Chickens",
-  //     itemDescription: "Lorem ipsum odor amet, consectetuer adipiscing elit. Inceptos primis pretium mus adipiscing lacus nascetur volutpat. Litora sollicitudin facilisis hac mi efficitur taciti risus torquent. Duis nascetur class magna mauris phasellus. Nam etiam ante tempor hac blandit ultricies nulla congue quis. Malesuada a lacus fermentum erat eros sodales. Laoreet orci parturient vivamus libero suscipit natoque. Lacus iaculis ultricies inceptos sed ac tempor. Sodales eros sem ante cursus enim eget ex orci. Nunc luctus lobortis sem orci id cursus morbi.",
-  //     itemImage: require("../../../img/items/test/05.jpg"),
-  //     bidAmount: 45263
-  //   }
-  // ];
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    const tokenObject = JSON.parse(storedToken);
 
-  // console.log(items[0].itemImage)
-
-const[items, setItems] = useState([]);
-const [loading, setLoading] = useState(true); // State to manage loading state
-const [error, setError] = useState(null); // State to manage errors
-
-useEffect(() => {
-  const storedToken = localStorage.getItem('token');
-  const tokenObject = JSON.parse(storedToken);
-
-  axios.get('http://localhost:8080/bid', {
-    headers: {
-      Authorization: `Bearer ${tokenObject.accessToken}`
-    }
-  })
-    .then(response => {
-      setItems(response.data); // Update the items state with the response data
-      setLoading(false); // Set loading to false once data is fetched
-      console.log(items);
+    // Fetch items
+    axios.get('http://localhost:8080/item', {
+      headers: {
+        Authorization: `Bearer ${tokenObject.accessToken}`
+      }
     })
-    .catch(err => {
-      setError(err); // Set the error state if there's an error
-      setLoading(false); // Set loading to false even if there's an error
-    });
-}, []);
+      .then(response => {
+        setItems(response.data); // Update the items state with the response data
+        return axios.get('http://localhost:8080/bid', { // Fetch bids
+          headers: {
+            Authorization: `Bearer ${tokenObject.accessToken}`
+          }
+        });
+      })
+      .then(response => {
+        setBids(response.data); // Update bids state with the fetched bids
+        setLoading(false); // Set loading to false once data is fetched
+      })
+      .catch(err => {
+        setError(err); // Set the error state if there's an error
+        setLoading(false); // Set loading to false even if there's an error
+      });
+  }, []);
 
-if (loading) return <p>Loading...</p>;
-if (error) return <p>Error loading items: {error.message}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error loading items: {error.message}</p>;
 
   return (
     <>
       <div className="text-center my-10">
         <h1 className="uppercase text-5xl">Auction House</h1>
-        <Button as={Link} to={`/newitem`} className="mt-6" color="secondary">Add New Item <PlusIcon className="w-5 h-5 ml-2" /></Button>
+        <Button as={Link} to={`/newitem`} className="mt-6" color="secondary">
+          Add New Item <PlusIcon className="w-5 h-5 ml-2" />
+        </Button>
       </div>
 
       <div className="flex flex-col items-center space-y-6 mb-10">
-        {items.map((item) => (
-          <Card
-            key={item.item.item_id}
-            isBlurred
-            className="border-none bg-background/60 dark:bg-default-100/50 max-w-[1000px]"
-            shadow="sm"
-          >
-            <CardBody className="flex items-center">
-              <div className="flex items-center">
-                <div className="flex-shrink-0">
-                  <Image
-                    isZoomed
-                    alt={item.item.itemName}
-                    className="object-cover"
-                    height={300}
-                    shadow="md"
-                    src={item.item.itemImage}
-                    width={300}
-                  />
+        {items.filter(item => item.approved).map((item) => {
+          // Find the highest bid for the current item
+          const itemBids = bids.filter(bid => bid.item.item_id === item.item_id);
+          const highestBid = itemBids.length > 0 ? Math.max(...itemBids.map(bid => bid.amount)) : 0;
+
+          return (
+            <Card
+              key={item.item_id}
+              isBlurred
+              className="border-none bg-background/60 dark:bg-default-100/50 max-w-[1000px]"
+              shadow="sm"
+            >
+              <CardBody className="flex items-center">
+                <div className="flex items-center">
+                  <div className="flex-shrink-0">
+                    <Image
+                      isZoomed
+                      alt={item.itemName}
+                      className="object-cover"
+                      height={300}
+                      shadow="md"
+                      src={item.itemImage}
+                      width={300}
+                    />
+                  </div>
+                  <div className="ml-6 flex flex-col">
+                    <div className="flex w-full mb-2 space-x-5 items-center">
+                      <Button as={Link} to={`/item/${item.item_id}`} className="w-1/3" color="success">Place Bid</Button>
+                      <Button as={Link} to={`/item/${item.item_id}`} className="w-1/3" color="primary">View Item <EyeIcon className="w-5 h-5 ml-2" /></Button>
+                      <p className="w-1/3 text-left">
+                        Current Bid: {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(highestBid)}
+                      </p>
+                    </div>
+                    <h2 className="text-3xl my-3">{item.itemName}</h2>
+                    <p className="text-l">{item.itemDescription}</p>
+                  </div>
                 </div>
-                <div className="ml-6 flex flex-col">
-                <div className="flex w-full mb-2 space-x-5 items-center">
-                  <Button className="w-1/3" color="success">Place Bid</Button>
-                  
-                  <Button as={Link} to={`/item/${item.item.item_id}`} className="w-1/3" color="primary">View Item <EyeIcon className="w-5 h-5 ml-2" /></Button>
-                  <p className="w-1/3 text-left">Current Bid: {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(item.amount)}</p>
-                </div>
-                <h2 className="text-3xl my-3">{item.item.itemName}</h2>
-                <p className="text-l">{item.item.itemDescription}</p>
-              </div>
-              </div>
-            </CardBody>
-          </Card>
-        ))}
+              </CardBody>
+            </Card>
+          );
+        })}
       </div>
     </>
   );
--- frontend/src/components/pages/singleitem/single-item.jsx
+++ frontend/src/components/pages/singleitem/single-item.jsx
@@ -1,8 +1,8 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Image, Button, Textarea, Card, CardBody } from "@nextui-org/react";
+import { Image, Button, Textarea, Card, CardBody, Input } from "@nextui-org/react";
 import { TrashIcon } from "@heroicons/react/24/outline";
-import { jwtDecode } from 'jwt-decode'
+import { jwtDecode } from 'jwt-decode';
 import axios from "axios";
 
 function SingleItem() {
@@ -10,15 +10,61 @@ function SingleItem() {
   const navigate = useNavigate(); // Hook to navigate programmatically
   const [item, setItem] = useState({});
   const [comments, setComments] = useState([]);
+  const [bids, setBids] = useState([]);
   const [commentContent, setCommentContent] = useState("");
+  const [currentUserId, setCurrentUserId] = useState(0);
+  const [highestBid, setCurrentHighestBid] = useState(0);
+  const [newBid, setNewBid] = useState(""); // Bid input state
+  const [isValidBid, setIsValidBid] = useState(false);
 
   const handleCommentChange = (e) => {
     setCommentContent(e.target.value);
   }
 
+  const handleNewBidChange = (e) => {
+    const bidValue = parseFloat(e.target.value);
+    setNewBid(bidValue);
+    setIsValidBid(bidValue > highestBid);
+  }
+
+  const handlePlaceBid = () => {
+    if (isValidBid) {
+      const storedToken = localStorage.getItem('token');
+      const tokenObject = JSON.parse(storedToken);
+      const decodedToken = jwtDecode(tokenObject.accessToken);
+      const userId = decodedToken.user_Id;
+
+      const bidData = {
+        bidtime: new Date().toISOString(),
+        amount: newBid,
+        user: { user_id: userId },
+        item: { item_id: parseInt(id) }
+      };
+
+      axios.post(`http://localhost:8080/bid`, bidData, {
+        headers: {
+          Authorization: `Bearer ${tokenObject.accessToken}`
+        }
+      })
+      .then(response => {
+        setBids([...bids, response.data]);
+        setCurrentHighestBid(newBid);
+        setNewBid(""); // Reset the input field after successful bid
+      })
+      .catch(error => {
+        console.error("Error placing bid", error);
+      });
+    }
+  };
+
+  // Preloads the item and retrieves it from the API
   useEffect(() => {
     const storedToken = localStorage.getItem('token');
     const tokenObject = JSON.parse(storedToken);
+    const decodedToken = jwtDecode(tokenObject.accessToken);
+    const userId = decodedToken.user_Id;
+
+    setCurrentUserId(userId);
 
     axios.get(`http://localhost:8080/item/${parseInt(id)}`, {
       headers: {
@@ -27,13 +73,14 @@ function SingleItem() {
     })
     .then(response => {
       setItem(response.data);
-    })
+    });
 
     if (!item) {
       navigate("/404"); // Navigate to the NotFound page
     }
   }, [item, navigate]);
 
+  // Preloads the comments attached to this item
   useEffect(() => {
     const storedToken = localStorage.getItem('token');
     const tokenObject = JSON.parse(storedToken);
@@ -44,58 +91,77 @@ function SingleItem() {
       }
     })
     .then(response => {
-      // Filter comments based on the current item id NOTE: This is not optimal and the backend should have an endpoint for this
-    const filteredComments = response.data.filter(comment => comment.item.item_id === parseInt(id));
-    setComments(filteredComments);
-
-    //TODO: Below is testing, delete afterwards
-    const decodedToken = jwtDecode(storedToken);
-    const userId = decodedToken.user_Id;
-    console.log("User ID: " + userId);
+      const filteredComments = response.data.filter(comment => comment.item.item_id === parseInt(id));
+      setComments(filteredComments);
     })
     .catch(error => {
       console.error("Error fetching comments:", error);
     });
   }, [id]);
 
-  const postComment = () => {
-    if(commentContent !== "")
-    {
-      const storedToken = localStorage.getItem('token');
+  // Preloads the bids and sets the highestBid to the currently highest bid on this item
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
     const tokenObject = JSON.parse(storedToken);
-    
-    const decodedToken = jwtDecode(tokenObject.accessToken);
-    const userId = decodedToken.user_Id;
-  
-    const commentData = {
-      comment_id: 20,
-      commenttime: new Date().toISOString(),
-      content: commentContent,
-      user: { user_id: userId },
-      item: { item_id: parseInt(id) }
-    };
-  
-    axios.post('http://localhost:8080/comment', commentData, {
+
+    axios.get(`http://localhost:8080/bid`, {
       headers: {
         Authorization: `Bearer ${tokenObject.accessToken}`
       }
     })
     .then(response => {
-      console.log("Comment submitted successfully:", response.data);
-      setComments([...comments, response.data]);
+      const filteredBids = response.data.filter(bid => bid.item.item_id === parseInt(id));
+      setBids(filteredBids);
+
+      // Find the highest bid amount
+      if (filteredBids.length > 0) {
+        const maxBid = Math.max(...filteredBids.map(bid => bid.amount));
+        setCurrentHighestBid(maxBid);
+      } else {
+        setCurrentHighestBid(0); // If no bids, set highest bid to 0
+      }
     })
     .catch(error => {
-      console.error("Error submitting comment:", error);
+      console.error("Error fetching bids:", error);
     });
+  }, [id]);
+
+  // Method that creates a new comment and posts it to the database
+  const postComment = () => {
+    if (commentContent !== "") {
+      const storedToken = localStorage.getItem('token');
+      const tokenObject = JSON.parse(storedToken);
+      const decodedToken = jwtDecode(tokenObject.accessToken);
+      const userId = decodedToken.user_Id;
+
+      const commentData = {
+        comment_id: 20,
+        commenttime: new Date().toISOString(),
+        content: commentContent,
+        user: { user_id: userId },
+        item: { item_id: parseInt(id) }
+      };
+
+      axios.post('http://localhost:8080/comment', commentData, {
+        headers: {
+          Authorization: `Bearer ${tokenObject.accessToken}`
+        }
+      })
+      .then(response => {
+        console.log("Comment submitted successfully:", response.data);
+        setComments([...comments, response.data]);
+        setCommentContent("");
+      })
+      .catch(error => {
+        console.error("Error submitting comment:", error);
+      });
     }
   };
 
-  const deleteComment = (commentId) =>
-  {
+  // Method that deletes a comment by its id
+  const deleteComment = (commentId) => {
     const storedToken = localStorage.getItem('token');
     const tokenObject = JSON.parse(storedToken);
-
-    console.log("Comment ID to Delete: " + commentId);
 
     axios.delete(`http://localhost:8080/comment/delete/${commentId}`, {
       headers: {
@@ -104,7 +170,6 @@ function SingleItem() {
     })
     .then(() => {
       console.log("Comment deleted successfully");
-      // Update the comments state by filtering out the deleted comment
       setComments(comments.filter(comment => comment.comment_id !== commentId));
     })
     .catch(error => {
@@ -136,9 +201,19 @@ function SingleItem() {
         </div>
         <div className="w-1/2">
           <div className="flex w-full mb-2 space-x-5 items-center">
-            <Button className="w-1/2" color="success">Place Bid</Button>
-            <p className="w-1/2 text-left">
-              {/* Current Bid: {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(content.bidAmount)} TOOD: FIX THIS */}
+            <Input
+              type="number"
+              label="New Bid"
+              value={newBid}
+              onChange={handleNewBidChange}
+              placeholder="Enter your bid"
+              className="w-1/2"
+            />
+            <Button className="w-1/4" color="success" onClick={handlePlaceBid} disabled={!isValidBid}>
+              Place Bid
+            </Button>
+            <p className="w-1/4 text-left">
+              Current Bid: {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(highestBid)}
             </p>
           </div>
           <p className="text-lg">{item.itemDescription}</p>
@@ -157,25 +232,29 @@ function SingleItem() {
         <Button onClick={postComment} className="w-1/4 mb-5" color="primary">Leave a Comment</Button>
       </div>
       <div style={{ width: '1000px' }} >
-      {comments.map((comment, index) => (
-      <Card key={index} className="mb-3 w-full">
-        <CardBody className="flex w-full flex-row items-center justify-between">
-          <div className="flex flex-col">
-            <span className="font-semibold">{comment.user.username}</span>
-            <span className="text-sm text-gray-600">
-              {new Date(comment.commenttime).toLocaleString()}
-            </span>
-            <span className="mt-2">{comment.content}</span>
-          </div>
-          <div className="justify-right">
-            <Button color="danger" className="flex items-center" onClick={() => deleteComment(comment.comment_id)}>
-              Delete <TrashIcon className="w-5 h-5 ml-2" />
-            </Button>
-          </div>
-        </CardBody>
-      </Card>
-    ))}
-
+        {comments.map((comment, index) => (
+          <Card key={index} className="mb-3 w-full">
+            <CardBody className="flex w-full flex-row items-center justify-between">
+              <div className="flex flex-col">
+                <span className="font-semibold">{comment.user.username}</span>
+                <span className="text-sm text-gray-600">
+                  {new Date(comment.commenttime).toLocaleString()}
+                </span>
+                <p>{comment.content}</p>
+              </div>
+              {currentUserId === comment.user.user_id || currentUserId === item.owner.user_id && (
+                <Button
+                  onClick={() => deleteComment(comment.comment_id)}
+                  className="ml-5"
+                  color="danger"
+                >
+                  Delete 
+                  <TrashIcon className="h-5 w-5" />
+                </Button>
+              )}
+            </CardBody>
+          </Card>
+        ))}
       </div>
     </div>
   );
